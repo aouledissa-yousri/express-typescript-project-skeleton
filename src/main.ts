89f9b5b1ad108dcc94b56cece88a87b03ac1a1c0
aouledissa <aouledissayousri@gmail.com>
2022-10-07 21:54:52 +0100

    added database connection configuration

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import express from "express"
+import cors from "cors"
 import { routes } from "./routes"
 
+//building application
 const app = express()
+
+//hosts that are allowed to connect to the application
+const corsOptions = []
+
 app.use(express.json())
+app.use(cors(corsOptions))
+
 
 let port = parseInt(process.argv[2])
 if(Number.isNaN(port)) port = 3000
 
-
+//main routes
 app.use("/", routes)
 
+
+//run express server
 app.listen(port, () => {
     console.log("Application is running on http://localhost:"+port)
 })
